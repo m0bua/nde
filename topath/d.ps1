9d0e44bdd47862ca27d1bbe912b7938838222f67
m0b <rn0b@live.com>
2020-06-12 16:28:23 +0300

    D scripts.

--- topath/d.ps1
+++ topath/d.ps1
@@ -1,59 +1,68 @@
-Function sudo ($command){
-  &{ Start-Process powershell -ArgumentList $command -Verb RunAs -WindowStyle hidden}
-}
-
-Function get_setting($name){
-  $content = Get-Content ("{0}\settings.yaml" -f (Split-Path -parent $PSScriptRoot))
-  return [regex]::match($content,"$name\: ([^\s\n]*)").Groups[1].Value
-}
-
-Function set_setting($name, $val){
-  if([System.IO.Directory]::Exists("{0}\settings.yaml" -f (Split-Path -parent $PSScriptRoot))){
-    $content = Get-Content ("{0}\settings.yaml" -f (Split-Path -parent $PSScriptRoot))
-    $old_val = [regex]::match($content,"$name\: ([^\s\n]*)").Value
-  }
-  if($old_val -and $val){$content = $content.replace("$old_val", ("{0}: {1}" -f $name, $val))}
-  else{$content += "{0}: {1}" -f $name, $val}
-  Set-Content -Path ("{0}\settings.yaml" -f (Split-Path -parent $PSScriptRoot)) -Value $content
-}
-
-Function first_config(){
-  $script_path = $PSScriptRoot;
-  if(($script_path -match '\\example\\topath$')){
-    $script_path += '\..\..\topath'
-    if(![System.IO.Directory]::Exists($script_path)){New-Item $script_path -ItemType "directory" > $null}
-    $script_path = Resolve-Path $script_path
-    if(![System.IO.File]::Exists("{0}\d.ps1" -f $script_path)){
-      sudo "New-Item -ItemType SymbolicLink -Path $script_path\d.ps1 -Target $PSScriptRoot\d.ps1"
-    }
-
-    while(![System.IO.File]::Exists("{0}\d.ps1" -f $script_path)) { Start-Sleep -Milliseconds 100 }
-    Invoke-Expression ("{0}\d.ps1 init" -f $script_path)
-    exit
-  }
-
-  echo "   NDE init!"
-
-  $path = [Environment]::GetEnvironmentVariable("path", [System.EnvironmentVariableTarget]::User)
-  if (!($path -match ($script_path -replace "\\", "\\")) ) {
-    [Environment]::SetEnvironmentVariable("path", "$path;$script_path", [System.EnvironmentVariableTarget]::User)
-  }
-
-  $prj_path = Read-Host "Put your projects folder path"
-  if([System.IO.Directory]::Exists($prj_path)){set_setting "prj_path" $prj_path}
-
-  exit
-}
-
-if(($PSScriptRoot -match '\\example\\topath$')){first_config}
-if(($args[0] -eq 'init') -or ($args[0] -eq 'config')){first_config}
+$default_container='php73'
 
 $dir = Split-Path -parent $PSScriptRoot;
+
+function run($exit=$true) {
+  Invoke-Expression "$command $params $extra_params"
+  if($exit){exit}
+}
 
 try {
   Push-Location
   Set-Location -Path  $dir
 
-  docker-compose $args | Tee-Object -Variable log
+  $command="docker-compose"
+  $params="$args"
+  $extra_params=""
 
-} finally {Pop-Location}
+  if ($params -eq '-purge') {
+    $command='docker'
+    echo " Killing all running containers:"
+    $extra_params=$(docker ps -q)
+    if ($extra_params -match '[\w\d]+' ){
+      $params="kill"; run($false)
+    } else { echo "   No containers to kill." }
+    echo " Removing all containers:"
+    $extra_params=$(docker ps -aq)
+    if ($extra_params -match '[\w\d]+' ){
+      $params="rm"; run($false)
+    } else { echo "   No containers to remove." }
+    echo " Removing all images:"
+    $extra_params=$(docker images -q)
+    if ($extra_params -match '[\w\d]+' ){
+      $params="rmi --force"; run($false)
+    } else { echo "   No images to remove." }
+    exit;
+  }
+
+  if ( $params -match 'halt' ){$params=($params -replace "halt", "down")}
+
+  if ( $params -match '^up.*-a'){$params=($params -replace "-a", "")}
+  elseif ($params -match '^up' -and (!($params -match '-d'))) {$extra_params='-d'}
+
+  if ( $params -eq 'df' ){ $command='docker'; $params='system df' }
+
+  if ( $params -eq '-kill') {
+    if ( $(docker ps -q) -match '[\w\d]+' ){$command='docker';$params="kill $(docker ps -q)"}
+    else {echo "   No containers to kill";exit}
+  }
+
+  if (!$args[0]){
+    $params = $(docker ps --format '{{.Names}}' | select-string "^nde-.*$default_container" | select -first 1)
+  } else {
+    $cont = $args[0]
+    $params = "$(docker ps --format '{{.Names}}' | select-string "^nde-.*$cont" | select -first 1)"
+  }
+
+  if ($params){ 
+    $command="docker exec -it";$extra_params="bash"
+    if ( $args[1] -match '[^\s]+' ){$extra_params=$args[1]}
+  } elseif (!"$(docker ps --format '{{.Names}}')") {
+    $params="up"; $extra_params="-d"
+  } else {
+    $params="$args"
+  }
+
+  run($true)
+
+} finally { Pop-Location }
--- topath/d.sh
+++ topath/d.sh
@@ -1,38 +1,49 @@
 #!/bin/bash
 default_container=php73
 
-run () { $command $params $extra_params; exit; }
+run () {
+  $command $params $extra_params
+  if [[ $exit == 'true' ]]; then exit; fi
+}
 cd $(dirname $(dirname $(realpath ${BASH_SOURCE[0]})))
-params="$*"
+
 command="docker-compose"
+params="$*"
+extra_params=""
+exit=true
 
-if [[ $params =~ halt ]]; then params=$(echo $params | sed 's/halt/down/'); run; exit; fi
+if [[ $1 == -purge ]]; then
+  command='docker'
+  echo " Killing all running containers:"
+  extra_params=$(docker ps -q)
+  if [[ $extra_params =~ [\w\d]+ ]]; then
+    params='kill'; exit='false'; run
+  else echo "   No containers to kill."; fi
+  echo " Removing all containers:"
+  extra_params=$(docker ps -aq)
+  if [[ $extra_params =~ [\w\d]+ ]]; then
+    params='rm'; exit='false'; run
+  else echo "   No containers to remove."; fi
+  echo " Removing all images:"
+  extra_params=$(docker images -q)
+  if [[ $extra_params =~ [\w\d]+ ]]; then
+    params='rmi --force'; exit='false'; run
+  else echo "   No images to remove."; fi
+  exit;
+fi
 
-if [[ $params =~ ^up.*-a ]]; then params=$(echo $params | sed 's/ -a//'); run; exit;
-else if [[ $params =~ ^up ]]; then if [[ ! $params =~ -d ]]; then extra_params='-d'; run; exit; fi; fi; fi
+if [[ $params =~ halt ]]; then params=$(echo $params | sed 's/halt/down/'); fi
+
+if [[ $params =~ ^up.*-a ]]; then params=$(echo $params | sed 's/ -a//')
+else if [[ $params =~ ^up ]]; then if [[ ! $params =~ -d ]]; then extra_params='-d'; fi; fi; fi
 
 if [[ $1 == df ]]; then docker system df; exit; fi
 
 if [[ $1 == -kill ]]; then
-  if [[ $params$(docker ps -q) =~ [\w\d]+ ]]; then docker kill $(docker ps -q)
-  else echo "   No containers to kill"
-fi; exit; fi
-
-if [[ $1 == -purge ]]; then
-  echo " Killing all running containers:"
-  params=$(docker ps -q)
-  if [[ $params =~ [\w\d]+ ]]; then docker kill $params
-  else echo "   No containers to kill."; fi
-  echo " Removing all containers:"
-  params=$(docker ps -aq)
-  if [[ $params =~ [\w\d]+ ]]; then docker rm $params
-  else echo "   No containers to remove."; fi
-  echo " Removing all images:"
-  params=$(docker images -q)
-  if [[ $params =~ [\w\d]+ ]]; then docker rmi --force  $params
-  else echo "   No images to remove."; fi
-  exit;
-fi
+  if [[ $(docker ps -q) =~ [\w\d]+ ]]; then 
+    $command='docker';$params="kill $(docker ps -q)"
+  else echo "   No containers to kill"; exit
+fi; fi
 
 if [[ -z $1 ]]; then params=$(docker ps --format {{.Names}} | grep -E "^nde-.*$default_container" | head -n 1)
 else params=$(docker ps --format {{.Names}} | grep -E "^nde-.*$1" | head -n 1); fi
