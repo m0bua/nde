222d7153e3b31fd16eb2cfc8fc045c1ba9eeaf6b
m0b <rn0b@live.com>
2020-06-17 18:30:33 +0300

    Linux script fixes.

--- topath/d.sh
+++ topath/d.sh
@@ -55,20 +55,24 @@ if [[ $1 == -kill ]]; then
   else echo "   No containers to kill"; exit
 fi; fi
 
-if [[ -z $1 ]]; then params=$(docker ps --format {{.Names}} 2> /dev/null | grep -E "^nde-.*$default_container" | head -n 1)
-else params=$(docker ps --format {{.Names}} 2> /dev/null | grep -E "^nde-.*$1" | head -n 1); fi
+if [[ -z $1 ]]; then params=$(docker ps --format {{.Names}} 2> /dev/null \
+  | grep -E "^nde-.*$default_container" | head -n 1)
+else params=$(docker ps --format {{.Names}} 2> /dev/null \
+  | grep -E "^nde-.*$1" | head -n 1); fi
 
 if [[ ! -z $params ]]; then
   command="docker exec -it"
   extra_params="bash"
   if [[ $2 =~ [^\s]+ ]]; then extra_params=$2; fi
-elif [[ -z "$(docker ps --format {{.Names}} 2> /dev/null)" ]]
-  then if [[ -z $1 ]];then params="up"; extra_params="-d"; fi
+elif [[ -z "$(docker ps --format {{.Names}} 2> /dev/null)" ]] \
+  && [[ -z $1 ]];then params="up"; extra_params="-d"
 else params="$*"; fi
 
 if [[ $1 == df ]]; then docker system df; exit; fi
-if [[ $params =~ halt ]]; then params=$(echo $params | sed 's/halt/down/'); fi
+if [[ $params =~ halt ]]
+  then params=$(echo $params | sed 's/halt/down/'); fi
 if [[ $params =~ ^up.*-a ]]; then params=$(echo $params | sed 's/ -a//')
-else if [[ $params =~ ^up ]]; then if [[ ! $params =~ -d ]]; then extra_params='-d'; fi; fi; fi
+elif [[ $params =~ ^up ]] && [[ ! $params =~ -d ]]
+  then extra_params='-d'; fi
 
 run
