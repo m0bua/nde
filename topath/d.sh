5c32b874b4555bc0d51e1d7191d44efe5179966a
m0b <git@m0b.in>
2024-08-01 18:10:15 +0300

    d -c ...

--- topath/d.sh
+++ topath/d.sh
@@ -2,9 +2,48 @@
 default_container=php
 
 run () {
-  $command $params $extra_params
+  if [[ $quoted_extra_params == 'true' ]]; then
+    $command $params "$extra_params"
+  else
+    $command $params $extra_params
+  fi
   if [[ $exit == 'true' ]]; then exit; fi
 }
+
+fn_purge () {
+  fn_delete
+  extra_params=$(docker images -q)
+  if [[ $extra_params =~ [\w\d]+ ]]; then
+    echo " Removing all images:"
+    params='rmi --force'; run
+  else echo "   No images to remove."; fi
+}
+
+fn_delete () {
+  fn_kill
+  extra_params=$(docker ps -aq)
+  if [[ $extra_params =~ [\w\d]+ ]]; then
+    echo " Removing all containers:"
+    params='rm'; run
+  else echo "   No containers to remove."; fi
+}
+
+fn_kill () {
+  fn_stop
+  command='docker'
+  exit='false'
+  extra_params=$(docker ps -q)
+  if [[ $extra_params =~ [\w\d]+ ]]; then
+    echo " Killing all running containers:"
+    params='kill'; run
+  else echo "   No containers to kill."; fi
+}
+
+fn_stop () {
+  echo " Stopping NDE"
+  params='down'; exit='false'; run
+}
+
 path=$(dirname $(dirname $(realpath ${BASH_SOURCE[0]})));
 config=${path}/docker-compose.yml
 
@@ -20,58 +59,18 @@ export GROUP_ID=$(id -g)
 command="docker-compose -f ${config}"
 params="$*"
 extra_params=""
+params_array=( "$@" )
 exit=true
+quoted_extra_params=false
 
 if [[ $params == '-init' ]]; then ./init.sh script; exit; fi
+if [[ $params == -purge ]]; then fn_purge; exit; fi
+if [[ $params == -delete ]]; then fn_delete; exit; fi
+if [[ $params == -del ]]; then fn_delete; exit; fi
+if [[ $params == -kill ]]; then fn_kill; exit; fi
+if [[ $params == df ]]; then docker system df; exit; fi
 
-if [[ $1 == -purge ]]; then
-  echo " Stopping NDE:"
-  params='down'; exit='false'; run
-  command='docker'
-  echo " Killing all running containers:"
-  extra_params=$(docker ps -q)
-  if [[ $extra_params =~ [\w\d]+ ]]; then
-    params='kill'; exit='false'; run
-  else echo "   No containers to kill."; fi
-  echo " Removing all containers:"
-  extra_params=$(docker ps -aq)
-  if [[ $extra_params =~ [\w\d]+ ]]; then
-    params='rm'; exit='false'; run
-  else echo "   No containers to remove."; fi
-  echo " Removing all images:"
-  extra_params=$(docker images -q)
-  if [[ $extra_params =~ [\w\d]+ ]]; then
-    params='rmi --force'; exit='false'; run
-  else echo "   No images to remove."; fi
-  exit;
-fi
-
-if [[ $1 == -delete ]]; then
-  echo " Stopping NDE:"
-  params='down'; exit='false'; run
-  command='docker'
-  echo " Killing all running containers:"
-  extra_params=$(docker ps -q)
-  if [[ $extra_params =~ [\w\d]+ ]]; then
-    params='kill'; exit='false'; run
-  else echo "   No containers to kill."; fi
-  echo " Removing all containers:"
-  extra_params=$(docker ps -aq)
-  if [[ $extra_params =~ [\w\d]+ ]]; then
-    params='rm'; exit='false'; run
-  else echo "   No containers to remove."; fi
-  exit;
-fi
-
-if [[ $1 == -kill ]]; then
-  echo " Stopping NDE:"
-  params='down'; exit='false'; run
-  if [[ $(docker ps -q) =~ [\w\d]+ ]]; then
-    command='docker';$params="kill $(docker ps -q)"
-  else echo "   No containers to kill"; exit
-fi; fi
-
-if [[ ! -z $1 ]]; then default_container=$1; fi
+if [[ $1 =~ ^[^-]+ ]]; then default_container=$1; fi
 params=$(docker ps --format {{.Names}} -f label=com.docker.compose.project=nde 2> /dev/null \
   | grep -E "$default_container" | sort | head -n 1)
 
@@ -82,9 +81,21 @@ if [[ ! -z $params ]]; then
 elif [[ -z $params ]] && [[ -z $1 ]];then params="up"; extra_params="-d"
 else params="$*"; fi
 
-if [[ $1 == df ]]; then docker system df; exit; fi
-if [[ $params =~ halt ]]
-  then params=$(echo $params | sed 's/halt/down/'); fi
+if [[ ${params_array[0]} == '-c' ]]; then
+  params="$params bash -c"
+  unset 'params_array[0]'
+  extra_params=${params_array[*]}
+  quoted_extra_params=true
+fi
+
+if [[ ${params_array[1]} == '-c' ]]; then
+  params="$params bash -c"
+  unset 'params_array[0]'
+  unset 'params_array[1]'
+  extra_params=${params_array[*]}
+  quoted_extra_params=true
+fi
+
 if [[ $params =~ ^up.*-a ]]; then params=$(echo $params | sed 's/ -a//')
 elif [[ $params =~ ^up ]] && [[ ! $params =~ -d ]]
   then extra_params='-d'; fi
