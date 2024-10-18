dc3de13c9e7de39a20384fe4e9d9e6ae2131afeb
m0b <git@m0b.in>
2024-10-18 05:43:54 +0000

    Xdebug config update & redis cache apply.

--- example/cfg/php/v.php
+++ example/cfg/php/v.php
@@ -12,11 +12,12 @@ $xdebugOff = 'develop';
 
 ### Code ###
 
-if (($_POST['cache'] ?? false) == 'clear') {
+if (($_POST['cache'] ?? null) == 'clear') {
     $redis = new Redis;
     $redis->connect($redisAddress, $redisPort);
     $redis->flushall();
-    header('Location: ' . $_SERVER['HTTP_HOST']);
+    header('Content-type: application/json');
+    echo '{"result":"ok"}';
     exit;
 }
 
@@ -34,9 +35,6 @@ $dirs = array_filter(
     }
 );
 
-$xDeb = function_exists('xdebug_info') && !empty(xdebug_info('mode'))
-    ? implode(',', xdebug_info('mode')) : 'off';
-
 $ch = curl_init('http:/localhost/containers/json');
 curl_setopt($ch, CURLOPT_UNIX_SOCKET_PATH, '/var/run/docker.sock');
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -48,21 +46,17 @@ $conteiners = array_map(function ($i) {
     return $i['Command'] === 'docker-php-entrypoint php-fpm' &&
         $i['Labels']['com.docker.compose.project'] = $prjName;
 }));
+sort($conteiners);
 $conList = array_map(function ($i) {
     return $i['Labels']['com.docker.compose.service'];
 }, $conData);
 
-sort($conteiners);
+if (function_exists('xdebug_info')) {
+    $xModes = ['develop', 'debug', 'coverage', 'trace', 'gcstats', 'profile'];
+    $xDebs = empty(xdebug_info('mode')) ? ['off'] : xdebug_info('mode');
+}
 
-foreach (
-    [
-        INFO_GENERAL,
-        INFO_CONFIGURATION,
-        INFO_VARIABLES,
-        INFO_ENVIRONMENT,
-        INFO_MODULES
-    ] as $block
-) {
+foreach ([INFO_GENERAL, INFO_CONFIGURATION, INFO_VARIABLES, INFO_ENVIRONMENT, INFO_MODULES] as $block) {
     ob_start();
     phpinfo($block);
     [$_, $body] = explode('<div class="center">', ob_get_contents());
@@ -76,6 +70,10 @@ $body = implode("\n", $phpInfos);
 <!DOCTYPE html>
 <html>
 
+<head>
+    <title>NDE <?= strtoupper($ver) ?> <?= phpversion() ?></title>
+</head>
+
 <body>
 
     <div id="header">
@@ -87,22 +85,32 @@ $body = implode("\n", $phpInfos);
             <?php endforeach ?>
         </div>
         <div class="right">
-            <?php if (!empty($conteiners)): ?>
-                <select>
+            <?php if (!empty($conteiners) && count($conteiners) > 1): ?>
+                <select id="containers">
                     <?php foreach ($conteiners as $conteiner): ?>
                         <option value="<?= $conteiner ?>" <?php if ($ver === $conteiner):
-                                                            ?> selected="selected" <?php endif ?>>
+                              ?> selected="selected" <?php endif ?>>
                             <?= $conteiner ?>
                         </option>
                     <?php endforeach ?>
                 </select>
             <?php endif ?>
-            <button id="xdebug">XDebug: <?= $xDeb ?></button>
+            <?php if (!empty($xModes)): ?>
+                <div id="xBlk">
+                    <button id="xdebug">Xdebug</button>
+                    <div id="xModes" class="hide">
+                        <?php foreach ($xModes as $mode): ?>
+                            <label>
+                                <input type="checkbox" name="xMode[]" value="<?= $mode ?>" <?php
+                                  if (in_array($mode, $xDebs)): ?>checked<?php endif ?>>
+                                <?= $mode ?>
+                            </label>
+                        <?php endforeach ?>
+                    </div>
+                </div>
+            <?php endif ?>
             <?php if (in_array($redisAddress, $conList)): ?>
-                <form method="post">
-                    <input type="hidden" name="cache" value="clear">
-                    <button>Redis cls</button>
-                </form>
+                <button id="redis">Redis cls</button>
             <?php endif ?>
             <button id="toggle">Show all</button>
         </div>
@@ -125,6 +133,7 @@ $body = implode("\n", $phpInfos);
 
         .right {
             text-align: center;
+            position: relative;
         }
 
         .right>form,
@@ -155,6 +164,35 @@ $body = implode("\n", $phpInfos);
 
         .right>form {
             margin-left: .7em;
+        }
+
+        .right>#xBlk {
+            display: inline-block;
+            position: relative;
+        }
+
+        .right>#xBlk>#xModes {
+            position: absolute;
+            background-color: #333e;
+            padding: .5em;
+            border-radius: .3em;
+            z-index: 10;
+            right: 0;
+            top: 100%;
+            top: calc(100% + 1em);
+        }
+
+        .right>#xBlk>#xModes>label {
+            display: block;
+            padding: .3em;
+            display: flex;
+            justify-content: left;
+            align-items: left;
+            word-wrap: none;
+        }
+
+        .right>#xBlk>#xModes>label>input {
+            margin-right: 1em;
         }
 
         #toggle {
@@ -251,12 +289,6 @@ $body = implode("\n", $phpInfos);
             display: table;
         }
 
-        div.center>table.hide,
-        div.center>h1.hide,
-        div.center>h2.hide {
-            display: none;
-        }
-
         div.center>h1.show,
         div.center>h2.show {
             display: block;
@@ -266,73 +298,77 @@ $body = implode("\n", $phpInfos);
             border: 1.5px solid;
             border-radius: .3em;
         }
+
+        .hide {
+            display: none !important;
+        }
     </style>
     <script>
-        let clickEls = '.center > table:first-child, .center > h2';
 
-        document.querySelectorAll('.center > h2 > a').forEach((el) => {
-            el.removeAttribute("href");
+        let containers = document.querySelector('#containers'),
+            xdebug = document.querySelector('#xdebug'),
+            xdebugInput = document.querySelectorAll('#xModes input'),
+            redis = document.querySelector('#redis'),
+            toggle = document.querySelector('#toggle'),
+            clickEls = document.querySelectorAll('.center > table:first-child, .center > h2');
+
+        if (containers) containers.addEventListener('change', (event) => {
+            domain = document.domain.split('.');
+            domain[domain.length - 2] = event.target.value;
+            window.location.href = window.location.href.replace(document.domain, domain.join('.'));
         });
 
-        document.querySelectorAll(clickEls).forEach((el) => {
-            el.addEventListener('click', (event) => toggle(event.target));
+        if (xdebug) xdebug.addEventListener('click', (event) => {
+            el = document.querySelector('#xModes');
+            if (el.classList.value.includes('hide'))
+                el.classList.remove('hide');
+            else el.classList.add('hide');
         });
 
-        document.querySelector('#toggle').addEventListener('click', (event) => {
-            let status = event.target.innerHTML == 'Show all';
-            document.querySelectorAll(clickEls).forEach((el) => {
-                toggle(el, status ? 'show' : 'hide');
-            });
-        });
-
-        document.querySelector('#header>.right>select').addEventListener('change', (event) => {
-            let href = window.location.href.split('.');
-            href[href.length - 2] = event.target.value;
-            window.location.href = href.join('.');
-        });
-
-        document.querySelector('#xdebug').addEventListener('click', (event) => {
-            let domain = window.location.hostname.split('.');
+        if (xdebugInput) xdebugInput.forEach((el) => el.addEventListener('click', (event) => {
+            let domain = window.location.hostname.split('.'),
+                els = document.querySelectorAll('#xModes input:checked'),
+                modes = Array.from(els, node => node.value),
+                cookie = modes.length > 0 ? modes.join(',') : 'off';
             domain[0] = '';
-
-            document.cookie = 'xdebug_mode=' +
-                (cookies().xdebug_mode == '<?= $xdebugOff ?>' ?
-                    '<?= $xdebugOn ?>' : '<?= $xdebugOff ?>') +
+            document.cookie = 'xdebug_mode=' + cookie +
                 '; domain=' + domain.join('.') + '; SameSite=Lax'
             window.location.href = window.location.href;
+        }));
+
+        if (redis) redis.addEventListener('click', (event) => {
+            if (confirm('Are you sure clearing all cache?')) {
+                formData = new FormData;
+                formData.append('cache', 'clear');
+                fetch(window.location.href, { method: "POST", body: formData })
+                    .then((res) => res.json())
+                    .then((json) => alert(json.result == 'ok' ? 'Done' : 'Error!'));
+            }
         });
 
-        function toggle(el, status = '') {
+        if (clickEls) clickEls.forEach((el) => el.addEventListener('click', (event) => toggleBlk(event.target)));
+
+        if (toggle) toggle.addEventListener('click', (event) => {
+            let status = event.target.innerHTML == 'Show all';
+            clickEls.forEach((el) => toggleBlk(el, status ? 'show' : 'hide'));
+        });
+
+        document.querySelectorAll('.center > h2 > a').forEach((el) => el.removeAttribute("href"));
+
+        function toggleBlk(el, status = '') {
             while (!el.parentElement.classList.contains('center')) el = el.parentElement;
-
             if (status.length == 0) status = el.classList.contains('open') ? 'hide' : 'show';
-
             if (status == 'hide') el.classList.remove('open');
             else el.classList.add('open');
-
             while (el.nextElementSibling && el.nextElementSibling.tagName == 'TABLE') {
                 el = el.nextElementSibling;
-                if (status.length == 0)
-                    status = el.classList.contains('show') ? 'hide' : 'show';
-
+                if (status.length == 0) status = el.classList.contains('show') ? 'hide' : 'show';
                 el.classList.remove(status == 'show' ? 'hide' : 'show');
                 el.classList.add(status);
-
-                if (status == 'show')
-                    document.querySelector('#toggle').innerHTML = 'Hide all';
-                else if (!document.querySelectorAll('.center > table:not(:first-child).show').length) {
+                if (status == 'show') document.querySelector('#toggle').innerHTML = 'Hide all';
+                else if (!document.querySelectorAll('.center > table:not(:first-child).show').length)
                     document.querySelector('#toggle').innerHTML = 'Show all';
-                }
             }
-        }
-
-        function cookies(key = null) {
-            let cookies = {};
-            document.cookie.split('; ').forEach(($i) => {
-                cookies[$i.split('=')[0]] = $i.split('=')[1];
-            });
-
-            return cookies[key] ?? cookies;
         }
     </script>
 
