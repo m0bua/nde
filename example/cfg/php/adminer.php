4be2c158c1539ae725d8d9eecf0149472505cecc
m0b <git@m0b.in>
2025-02-24 08:05:17 +0000

    Adminer update.

--- example/cfg/php/adminer.php
+++ example/cfg/php/adminer.php
@@ -1,5 +1,7 @@
 <?php
 
+$file = 'latest-mysql-en.php';
+
 error_reporting(0);
 ini_set('display_errors', 0);
 ini_set('display_startup_errors', 0);
@@ -8,20 +10,17 @@ ini_set('post_max_size', '10G');
 ini_set('upload_max_filesize', '10G');
 ini_set('max_execution_time', 360);
 
-$file = 'latest-mysql-en.php';
-if (!is_file($file) || !filesize($file))
+if (!filesize($file) || date_create('@'. filemtime($file)) < date_create('-1 week'))
   file_put_contents($file, strtr(file_get_contents("https://adminer.org/$file"), [
-
     'value="\'.h(SERVER).\'"' =>
     'value="\'.h($_GET["server"]??$_ENV["ADMINER_DEFAULT_SERVER"]??"localhost").\'"',
 
     'value="\'.h($_GET["username"]).\'"' =>
     'value="\'.h($_GET["username"]??$_ENV["ADMINER_DEFAULT_USER"]??"").\'"',
 
-    'auth[password]"' =>
-    'auth[password]" value="\'.h(empty($_GET["table"])'
-      .'?($_ENV["ADMINER_DEFAULT_PASSWORD"]??""):"").\'"',
-
+    'auth[password]"' => 'auth[password]" value="\'.h(empty($_GET["table"])'
+      . '?($_ENV["ADMINER_DEFAULT_PASSWORD"]??""):"").\'"',
   ]));
 
-if (is_file($file) || !!filesize($file)) require_once $file;
+if ((bool)filesize($file)) require_once $file;
+else echo 'Adminer file error!';
