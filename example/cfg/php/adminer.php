b882410a9d6eb145f53bc31cb2a1fc808d119ad7
m0b <git@m0b.in>
2025-04-08 10:31:37 +0000

    Adminer update fix.

--- example/cfg/php/adminer.php
+++ example/cfg/php/adminer.php
@@ -10,19 +10,26 @@ ini_set('post_max_size', '10G');
 ini_set('upload_max_filesize', '10G');
 ini_set('max_execution_time', 360);
 
-if (!filesize($file)) update($file);
-else {
-  preg_match('/\$ga="([^"]+)"/', file_get_contents($file), $matches);
-  if (version_compare($_COOKIE['adminer_version'], $matches[1] ?? 0))
-    update($file);
+if (!filesize($file)) {
+  update($file);
+} else {
+  preg_match('#^\* \@version ([\d\.]+)$#m', file_get_contents($file), $matches);
+  if (!empty($matches[1]) && version_compare($matches[1], $_COOKIE['adminer_version'], '<')) {
+    echo "<span style=color:red>Updating: {$matches[1]}->{$_COOKIE['adminer_version']}</span>";
+    update($file, $_COOKIE['adminer_version']);
+  }
 }
 
 if ((bool)filesize($file)) require_once $file;
 else echo 'Adminer file error!';
 
-function update($file)
+function update($file, $ver = null)
 {
-  file_put_contents($file, strtr(file_get_contents("https://adminer.org/$file"), [
+  $url = empty($ver) ? "https://adminer.org/$file" :
+    "https://github.com/vrana/adminer/releases/download/v$ver/"
+    . str_replace('latest', "adminer-$ver", $file);
+  $data = file_get_contents($url);
+  if (!empty($data)) file_put_contents($file, strtr($data, [
     'value="\'.h(SERVER).\'"' =>
     'value="\'.h($_GET["server"]??$_ENV["ADMINER_DEFAULT_SERVER"]??"localhost").\'"',
 
