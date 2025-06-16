12689002b5e8aa5deef044f8b1b2b3892d5fe308
m0b <git@m0b.in>
2025-06-16 07:12:10 +0000

    Docker-compose install script update.

--- docker.sh
+++ docker.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
-#Refresh and install packages
-sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common libssl-dev libffi-dev git wget nano
+for pkg in $(dpkg-query -W -f='${binary:Package}\n' | grep docker); do sudo apt-get purge -y $pkg; done
+for pkg in containerd containerd.io runc; do sudo apt-get purge -y $pkg; done
+sudo apt autoremove -y
 
-#Add user group
 sudo groupadd docker
 sudo usermod -aG docker ${USER}
 
-#Add docker key and repo
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common libssl-dev libffi-dev git wget nano
+
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
 
-#Install docker and docker-compose
-sudo apt-get install -y docker-ce containerd.io docker-compose
-
-#Install docker-compose (if the previous command failed to install)
-sudo curl -sSL https://github.com/docker/compose/releases/download/v`curl -s https://github.com/docker/compose/tags | grep "compose/releases/tag" | sed -r 's|.*([0-9]+\.[0-9]+\.[0-9]+).*|\1|p' | head -n 1`/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose && sudo chmod +x /usr/local/bin/docker-compose
+sudo apt-get install -y docker-compose-v2
