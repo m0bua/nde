8519a5bae6f33b1c0b2612997b43ebce9c76539f
m0b <git@m0b.in>
2026-08-11 13:49:25 +0000

    Adminer script update.

--- www/adminer.php
+++ www/adminer.php
@@ -10,9 +10,10 @@ ini_set('post_max_size', '10G');
 ini_set('upload_max_filesize', '10G');
 ini_set('max_execution_time', 360);
 
-if (!filesize($file)) {
-  update($file);
-} else {
+define('Adminer\\SERVER', $_ENV['ADMINER_DEFAULT_SERVER'] ?? '');
+
+if (!filesize($file)) update($file);
+else {
   preg_match('#^\* \@version ([\d\.]+)$#m', file_get_contents($file), $matches);
   if (!empty($matches[1]) && version_compare($matches[1], $_COOKIE['adminer_version'], '<')) {
     echo "<span style=color:red>Updating: {$matches[1]}->{$_COOKIE['adminer_version']}</span>";
@@ -20,8 +21,43 @@ if (!filesize($file)) {
   }
 }
 
-if ((bool)filesize($file)) require_once $file;
-else echo 'Adminer file error!';
+if ((bool)filesize($file)) {
+  ob_start();
+  register_shutdown_function(function () {
+    $html = ob_get_clean();
+    if (empty($_GET['file'])) {
+      preg_match('~<script\s+nonce="([^"]+)"~i', $html, $matches);
+      $nonce = $matches[1] ?? '';
+      $nonceAttribute = $nonce ? ' nonce="'
+        . htmlspecialchars($nonce, ENT_QUOTES) . '"' : '';
+      $username = env('USER');
+      $password = env('PASSWORD');
+      $script = <<<HTML
+<script$nonceAttribute>
+window.addEventListener('load', function () {
+  const params = new URLSearchParams(window.location.search)
+  if(!params.get('username')) {
+    ff('username', $username);
+    ff('password', $password);
+  }
+});
+function f(key) {
+    return document.querySelector('input[name="auth['+key+']"]');
+}
+function ff(key, val) {
+    const field = f(key);
+    if (field && !field.value) field.value = val;
+}
+</script>
+HTML;
+      $html .= $script;
+    }
+
+    echo $html;
+  });
+
+  require_once $file;
+} else echo 'Adminer file error!';
 
 function update($file, $ver = null)
 {
@@ -29,18 +65,15 @@ function update($file, $ver = null)
     "https://github.com/vrana/adminer/releases/download/v$ver/"
     . str_replace('latest', "adminer-$ver", $file);
   $data = file_get_contents($url);
-  if (!empty($data)) file_put_contents($file, strtr($data, [
-    'value=\'".h(SERVER)."\'' =>
-    'value=\"".h($_ENV["ADMINER_DEFAULT_SERVER"]??"localhost")."\"',
+  if (!empty($data)) file_put_contents($file, $data);
+}
 
-    'value="\'.h($_GET["username"]).\'"' =>
-    'value="\'.h($_ENV["ADMINER_DEFAULT_USER"]??"").\'"',
+function env(string $key)
+{
+  $key = strtoupper($key);
 
-    'auth[password]"' => 'auth[password]"'
-      . ' value="\' . h($_ENV["ADMINER_DEFAULT_PASSWORD"] ?? ""). \'"',
-
-    '"↑"' => '"🡅"',
-    '"↓"' => '"🡇"',
-    '"x"' => '"❌"',
-  ]));
+  return json_encode(
+    $_ENV["ADMINER_DEFAULT_$key"] ?? '',
+    JSON_HEX_TAG | JSON_HEX_AMP | JSON_HEX_APOS | JSON_HEX_QUOT
+  );
 }
